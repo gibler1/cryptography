b96dde0c6c4cdbe215deb5a5e65bef1f0852a681
segun adefemi <adefemisegun0@gmail.com>
2024-03-19 14:20:56 +0000

    MD4 implementations

--- hashing algos/hash.cs
+++ hashing algos/hash.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 class Hashfunctions
 {
-	static public void MD4(string message)
+	static public String MD4(string message)
 	{
 		List<byte> words = Encoding.ASCII.GetBytes(message).ToList();//turn string into byte format to make it easier to work with
 		foreach (byte wor in words)//testing loop
@@ -39,6 +39,8 @@ class Hashfunctions
 		words.AddRange(bitrep.GetRange(4, 4));//add the low-order bits first
 		words.AddRange(bitrep.GetRange(0, 4));
 
+		long N = words.Count/4;
+
 		//Step 3: Initialising message digest(MD) buffer
 		Word A = new Word(1, 35, 69, 103);
 		Word B = new Word(137, 171, 205, 239);
@@ -59,15 +61,120 @@ class Hashfunctions
 		}
         Word g(Word x, Word y, Word z)
         {
-            return (x.and(y)).or((x.complement()).and(z));
+            return ((x.and(y)).or(x.and(z))).or(y.and(z));
+        }
+        Word h(Word x, Word y, Word z)
+        {
+            return (x.xor(y)).xor(z);
         }
 
+		//Loop to iterate over blocks:
+		for (int i = 0; i < N / 16; i++)
+		{
+			//initialise the variables which will be used in this block
+			Word[] X = new Word[16];
+			for (int j = 0; j < 16; j++)
+			{
+				X[j] = new Word(words[(i * 16 + j) * 4], words[(i * 16 + j) * 4 + 1], words[(i * 16 + j) * 4 + 2], words[(i * 16 + j) * 4 + 3]);
+            }
+			Word AA = (Word)A.Clone();
+			Word BB = (Word)B.Clone();
+			Word CC = (Word)C.Clone();
+			Word DD = (Word)D.Clone();
+
+            //Round 1
+            //r1 represents [A B C D i s] which denotes: A = (A+ f(B,C,D) + X[i]) <<< s 
+            void r1(ref Word f1,Word f2, Word f3, Word f4, int a, int s)
+			{
+				Word temp = (X[a].add((A.add(f(B, C, D)))));
+				temp.shift(s);
+				f1 = temp;
+			}
+			r1(ref A, B, C, D, 0, 3);
+			r1(ref D, A, B, C, 1, 7);
+			r1(ref C, D, A, B, 2, 11);
+			r1(ref B, C, D, A, 3, 19);
+			r1(ref A, B, C, D, 4, 3);
+			r1(ref D, A, B, C, 5, 7);
+			r1(ref C, D, A, B, 6, 11);
+			r1(ref B, C, D, A, 7, 19);
+			r1(ref A, B, C, D, 8, 3);
+			r1(ref D, A, B, C, 9, 7);
+			r1(ref C, D, A, B, 10, 11);
+			r1(ref B, C, D, A, 11, 19);
+			r1(ref A, B, C, D, 12, 3);
+			r1(ref D, A, B, C, 13, 7);
+			r1(ref C, D, A, B, 14, 11);
+			r1(ref B, C, D, A, 15, 19);
+
+            //Round 2
+            //r2 represents [A B C D i s] which now denotes: A = (A+ g(B,C,D) + X[i] + 5A827999) <<< s 
+            void r2(ref Word f1, Word f2, Word f3, Word f4, int a, int s)
+            {
+				Word constant = new Word((byte)90, (byte)130, (byte)121, (byte)153);
+                Word temp = (X[a].add((A.add(g(B, C, D))))).add(constant);
+                temp.shift(s);
+                f1 = temp;
+            }
+            r2(ref A, B, C, D, 0, 3);
+            r2(ref D, A, B, C, 4, 5);
+            r2(ref C, D, A, B, 8, 9);
+            r2(ref B, C, D, A, 12, 13);
+            r2(ref A, B, C, D, 1, 3);
+            r2(ref D, A, B, C, 5, 5);
+            r2(ref C, D, A, B, 9, 9);
+            r2(ref B, C, D, A, 13, 13);
+            r2(ref A, B, C, D, 2, 3);
+			r2(ref D, A, B, C, 6, 5);
+            r2(ref C, D, A, B, 10, 9);
+            r2(ref B, C, D, A, 14, 13);
+            r2(ref A, B, C, D, 3, 3);
+            r2(ref D, A, B, C, 7, 5);
+            r2(ref C, D, A, B, 11, 9);
+            r2(ref B, C, D, A, 15, 13);
+
+            //Round 3
+            //r3 represents [A B C D i s] which now denotes: A = (A+ h(B,C,D) + X[i] + 6ED9EBA1) <<< s
+            void r3(ref Word f1, Word f2, Word f3, Word f4, int a, int s)
+            {
+                Word constant = new Word((byte)110, (byte)217, (byte)235, (byte)161);
+                Word temp = (X[a].add((A.add(h(B, C, D))))).add(constant);
+                temp.shift(s);
+                f1 = temp;
+            }
+            r3(ref A, B, C, D, 0, 3);
+            r3(ref D, A, B, C, 8, 5);
+            r3(ref C, D, A, B, 4, 9);
+            r3(ref B, C, D, A, 12, 15);
+            r3(ref A, B, C, D, 2, 3);
+            r3(ref D, A, B, C, 10, 9);
+            r3(ref C, D, A, B, 6, 11);
+            r3(ref B, C, D, A, 14, 15);
+            r3(ref A, B, C, D, 1, 3);
+            r3(ref D, A, B, C, 9, 9);
+            r3(ref C, D, A, B, 5, 11);
+            r3(ref B, C, D, A, 13, 15);
+            r3(ref A, B, C, D, 3, 3);
+            r3(ref D, A, B, C, 11, 9);
+            r3(ref C, D, A, B, 7, 11);
+            r3(ref B, C, D, A, 15, 15);
+
+			/*END of rounds*/
+			//final block additions
+			A = A.add(AA);
+			B = B.add(BB);
+			C = C.add(CC);
+			D = D.add(DD);
+        }
+		//returning the 128-bit sequence in hexadecimal format
+		return (A.toHexString()+B.toHexString()+C.toHexString()+D.toHexString());
+		
     }
 }
 /*class to allow for easy bitwise operations on 32bits.
- *methods:xor, and, or, complement
+ *methods:xor, and, or, complement, add,clone
  */
-public class Word
+public class Word : ICloneable
 {
 	private byte[] bits = new byte[4];
 	public Word(byte first, byte second, byte third, byte fourth)
@@ -113,4 +220,33 @@ public class Word
         Word newword = new Word(newbytes[0], newbytes[1], newbytes[2], newbytes[3]);
         return newword;
     }
+
+	//shifts the bits of this word by a given value - this changes the object state
+	public void shift(int s)
+	{
+		uint combined = BitConverter.ToUInt32(bits, 0);
+		combined = (combined << s | combined >> (32 - s));
+		bits = BitConverter.GetBytes(combined);
+	}
+
+	//adds 2 words together and returns the result without changing state
+	public Word add(Word other)
+	{
+        uint thisbytes = BitConverter.ToUInt32(bits, 0);
+        uint otherbytes = BitConverter.ToUInt32(other.bits, 0);
+		byte[] result = BitConverter.GetBytes(thisbytes + otherbytes);
+		return new Word(result[0], result[1], result[2], result[3]);
+    }
+
+	//Turns the word into its hexadecimal string equivalent
+	public String toHexString()
+	{
+        return BitConverter.ToString(bits).Replace("-", "");
+    }
+
+    //method to allow for easy copying of words
+    public object Clone()
+	{
+		return new Word(bits[0], bits[1], bits[2], bits[3]);
+	}
 }
